2470c38f3aff21f4e49ed377d2c96ad10f9d77b6
Brito <101473345+NickBritoDev@users.noreply.github.com>
2024-07-11 10:34:19 -0300

    feat: layout e design system criados

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Flex, Text } from "@chakra-ui/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Flex>
+      <Text>hello word</Text>
+    </Flex>
+  );
+}
